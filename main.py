430b9eaa8e90fdd6c2200109fc91694969a99030
RafaNararya <mrafanararya@gmail.com>
2026-04-15 21:43:13 -0500

    Added Making Graphs and Testing on Noise Removal

--- main.py
+++ main.py
@@ -26,19 +26,56 @@ def main():
     print(f"Noisy test file is made at {noisy_out}")
 
     # Decompose the signal
-    wavelet_type = 'db20'
-    decomposed_coeffs = pywt.wavedec(noisy_data, wavelet_type, level=9)
+    wavelet_type = 'db8'
+    decomposed_coeffs = pywt.wavedec(noisy_data, wavelet_type, level=5)
+
+    sigma = np.median(np.abs(decomposed_coeffs[-1])) / .6745
+    threshold_value  = sigma * np.sqrt(2 * np.log(len(noisy_data)))
+    denoised_coeffs = [decomposed_coeffs[0]]
+    for i in range(1, len(decomposed_coeffs)):
+        denoised_coeffs.append(pywt.threshold(decomposed_coeffs[i], value= (threshold_value * .2), mode = 'soft'))
     
     # Denoise signal
-    decomposed_coeffs = threshold(decomposed_coeffs, 0.035, 5)
+    #decomposed_coeffs = threshold(decomposed_coeffs, threshold_value, start = 1)
 
     # Reconstruct signal
-    denoised_data = pywt.waverec(decomposed_coeffs, wavelet_type)
+    denoised_data = pywt.waverec(denoised_coeffs, wavelet_type)
     print(f"Noisy SNR = {snr(clean_data, noisy_data)} dB")
     print(f"Denoised SNR = {snr(clean_data, denoised_data)} dB")
 
     save_audio(processed_out, fs, denoised_data)
     print(f"Saved cleaned audio to {processed_out}")
+
+
+
+    #Plots and Graphs
+    plot_dir = "graphsNMore"
+    os.makedirs(plot_dir, exist_ok=True)
+    
+    plt.figure(figsize=(12,10))
+
+    plt.subplot(2, 1, 1)
+    plt.plot(clean_data[10000:15000], label = "Clean", alpha = .7)
+    plt.plot(noisy_data[10000:15000], label = "Noisy", alpha = .5)
+    plt.title("Time: Clean vs Noisy")
+    plt.legend()
+
+    plt.subplot(2, 1, 2)
+    plt.plot(clean_data[10000:15000], label = "Clean Ground Truth", color = "gray", alpha = .3)
+    plt.plot(denoised_data[10000:15000], label = "Denoised Wavelet", color = "orange")
+    plt.title(f"Denoised Result (SNR : {snr(clean_data, denoised_data): .2f} dB)")
+    plt.legend()
+
+    plt.tight_layout()
+    plt.savefig(f"{plot_dir}/waveform_comparison.png")
+    #plt.show()
+
+    plt.figure(figsize=(10,6))
+    plt.specgram(denoised_data, Fs = fs)
+    plt.title("Spectrogram of Denoised Audio")
+    plt.ylabel("Frequency (Hz)")
+    plt.xlabel("Time (s)")
+    plt.savefig(f"{plot_dir}/spectrogram_denoised.png")
 
 if __name__ == "__main__":
     main()
